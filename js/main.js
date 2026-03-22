9a330ae795177d126355c4ca7a31808a922f4094
Bibrato Sarkar <82262740+bibrato-sarkar@users.noreply.github.com>
2026-03-22 18:09:53 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(".hamburger-button").on('click', function(){
+    $(".menu-block").toggle(100);
+    // $(".close-button").show(100);
+
+
+});
+$(".close-button").on('click', function(){
+    $(".menu-block").hide(500);
+});
